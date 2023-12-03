fcbb24c8b2e10f3b67b16d63aef7fab45cd7a1d7
stephaniebernhard <fanny.bernhard@gmail.com>
2023-12-03 13:40:06 +0100

    solved day 3 part 1 - don't look, it's ugly

--- 2023/day3.py
+++ 2023/day3.py
@@ -31,8 +31,6 @@ def manipulate_input(input):
 
 
 input = manipulate_input(input)
-print(input)
-
 
 def is_symbol(char):
     symbol = True
@@ -44,13 +42,10 @@ def is_symbol(char):
         symbol = False
     return symbol
 
-
-#todo handle first and last line separately
-
-#todo iterate through all lines in between
+# get all numbers from a line
 def get_all_numbers(input):
     arr = []
-    for line_number in range(number_of_lines):
+    for line_number in range(len(input)):
         line = input[line_number]
         current_number = ""
         number_array = []
@@ -69,34 +64,56 @@ def get_all_numbers(input):
 
 all_numbers = get_all_numbers(input)
 
-for line_number in range(number_of_lines):
+total_sum = 0
+
+# check if symbol around number for each number
+# wow, never seen code as ugly as this before...
+for line_number in range(1,len(input)-1):
     line_sum_parts = 0
     line = input[line_number]
     numbers = all_numbers[line_number]
-    if(line_number==0):
-        print("line1")
-        for number in all_numbers[0]:
-            is_part_num = False
-            number_length = len(number)
-            pos=line.find(number)
-            #check left diagonal
-            left_diag = input[line_number+1][pos-1]
-            if (is_symbol(left_diag)):
+    last_pos = 0
+    for number in all_numbers[line_number]:
+        is_part_num = False
+        number_length = len(number)
+        pos=line.find(number, last_pos)
+        last_pos = pos + len(number)
+        #check lower left diagonal
+        left_diag = input[line_number+1][pos-1]
+        if (is_symbol(left_diag)):
+            is_part_num = True
+        #check lower middle
+        for i in range(number_length):
+            below = input[line_number+1][pos+i]
+            if(is_symbol(below)):
                 is_part_num = True
-            #check middle
-            for i in range(number_length):
-                below = input[line_number+1][pos+i]
-                if(is_symbol(below)):
-                    is_part_num = True
-            #check right diagonal
-            right_diag = input[line_number+1][pos+number_length]
-            if(is_symbol(right_diag)):
+        #check lower right diagonal
+        right_diag = input[line_number+1][pos+number_length]
+        if(is_symbol(right_diag)):
+            is_part_num = True
+        #check front
+        front = input[line_number][pos-1]
+        if(is_symbol(front)):
+            is_part_num = True
+        #check back
+        back = input[line_number][pos+number_length]
+        if(is_symbol(back)):
+            is_part_num = True
+        #check higher left diag
+        left_diag_up = input[line_number-1][pos-1]
+        if (is_symbol(left_diag_up)):
+            is_part_num = True
+        #check higher middle
+        for i in range(number_length):
+            upper = input[line_number-1][pos+i]
+            if(is_symbol(upper)):
                 is_part_num = True
-            if (is_part_num):
-                line_sum_parts = line_sum_parts + int(number)      
-        print("sum of part nums: "+ str(line_sum_parts)) 
-    elif(line_number==(number_of_lines)):
-        print("line"+str(line_number+1))
-    else:
-        print("line"+str(line_number+1))
-    
+        #check higher right
+        right_diag_up = input[line_number-1][pos+number_length]
+        if(is_symbol(right_diag_up)):
+            is_part_num = True
+        if(is_part_num):
+            line_sum_parts = line_sum_parts + int(number) 
+    total_sum = total_sum + line_sum_parts
+
+print(total_sum)
