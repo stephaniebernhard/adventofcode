3e92a3e6183f68323381db62eabd24d1962b8e96
stephaniebernhard <fanny.bernhard@gmail.com>
2023-12-06 17:13:43 +0100

    solved day 5 without memory error, see day5new

--- 2023/day5new.py
+++ 2023/day5new.py
@@ -1,80 +1,45 @@
 from helpers import read_cwd
 
 cwd = read_cwd()
-            
+
 def import_input(filename):
-    my_list = []
     file_path = cwd+filename
+    seeds = []
+    input_arr = []
+    map = []  
     with open(file_path,"r") as f:
-        for line in f:
-            my_list.append(line.rstrip('\n')) 
-    return my_list
+        for row in f:
+            line = (row.rstrip('\n')) 
+            if line == "":
+                pass
+            elif line.startswith("seeds"):
+                seeds = [int(n) for n in line.split() if n.isdigit()]
+            elif line[0].isalpha():
+                if(map!=[]):
+                    input_arr.append(map)
+                map = []
+            else:
+                map.append([int(n) for n in line.split() if n.isdigit()])
+        else:
+            input_arr.append(map)
+    return seeds, input_arr
 
-input = import_input("day5.txt")
+seeds, input_arr = import_input("day5.txt")
+   
+def loc_for_seed(start, input_arr):
+    thing = start
+    for i in range(len(input_arr)):
+        for source in input_arr[i]:
+            if thing in range(source[1],source[1]+source[2]):
+                diff = thing-source[1]
+                thing = source[0]+diff
+                break
+    return thing
 
-seeds = []
-stos = {}
-stof = {}
-ftow = {}
-wtol = {}
-ltot = {}
-ttoh = {}
-htol = {}
-curr_map = {}
-
-for line in input:
-    if line.startswith("seeds"):
-        seeds = line[7:].split(" ")
-    if line.startswith("seed-to"):
-        curr_map = stos
-    if line.startswith("soil"):
-        curr_map = stof
-    if line.startswith("fertilizer"):
-        curr_map = ftow
-    if line.startswith("water"):
-        curr_map = wtol
-    if line.startswith("light"):
-        curr_map = ltot
-    if line.startswith("temperature"):
-        curr_map = ttoh
-    if line.startswith("humidity"):
-        curr_map = htol
-    if (line =="" or not line[0].isdigit()):
-        pass
-    else: 
-        line_values = line.split(" ")
-        destination = int(line_values[0])
-        source = int(line_values[1])
-        length = int(line_values[2])
-        for i in range(length):
-            curr_map[source + i] = destination + i
-
-loc_map = {}
+locs = []
 
 for seed in seeds:
-    seed = int(seed)
-    soil = seed
-    if(seed in stos.keys()):
-        soil = stos[seed]
-    fert = soil
-    if(soil in stof.keys()):
-        fert = stof[soil]
-    water = fert
-    if(fert in ftow.keys()):
-        water = ftow[fert]
-    light = water
-    if(water in wtol.keys()):
-        light = wtol[water]
-    temp = light
-    if(light in ltot.keys()):
-        temp = ltot[light]
-    hum = temp
-    if(temp in ttoh.keys()):
-        hum = ttoh[temp]
-    loc = hum
-    if(hum in htol.keys()):
-        loc = htol[hum]
-    loc_map[seed] = loc
-    
-lowest_loc = min(loc_map.values())
-print(lowest_loc)
+    locs.append(loc_for_seed(seed,input_arr))
+
+print(min(locs))
+
