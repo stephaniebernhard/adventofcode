7665f1755d7dda7d46090f8fd2e73bb47512307f
stephaniebernhard <fanny.bernhard@gmail.com>
2023-12-11 09:51:12 +0100

    solved day 11 part 2

--- 2023/day11p2.py
+++ 2023/day11p2.py
@@ -11,19 +11,15 @@ def import_input(filename):
     return my_list
 
 input = import_input("day11.txt")
+factor = 1000000
 
-#expand vertically + padding magic to avoid index out of range
-def manipulate_input(input):
-    new_line = ""
-    for index in range(len(input[0])):
-        new_line = new_line + "."
-    input_new = []
-    for line in input:
-        input_new.append(line)
-        if(line==new_line):
-            input_new.append(new_line)
-    #todo expand columns
-    return input_new
+#get indices where horizontal lines need to be inserted
+def get_vertical_indices(input):
+    add_indices = []
+    for i in range(len(input)):
+        if(input[i].count(".") == len(input[i])):
+            add_indices.append(i)
+    return add_indices
 
 # get indices where vertical lines need to be inserted
 def get_horizontal_indices(input):
@@ -37,22 +33,8 @@ def get_horizontal_indices(input):
             add_indices.append(pos)
     return add_indices
 
-# expand horizontally
-def expand(input, indices):
-    new_input = []
-    for line in input:
-        new_line = ""
-        for pos in range(len(line)):
-            if pos in indices:
-                new_line = new_line + ".."
-            else:
-                new_line = new_line + line[pos]
-        new_input.append(new_line)
-    return new_input
-
-input = manipulate_input(input)
-indices = get_horizontal_indices(input)
-input = expand(input, indices)
+hindices = get_horizontal_indices(input)
+vindices = get_vertical_indices(input)
 galaxies = []
 
 # get positions of galaxies
@@ -60,12 +42,24 @@ for index in range(len(input)):
     for pos in range(len(input[index])):
         if input[index][pos] == '#':
             galaxies.append([index, pos])
-
 total_sum = 0
 
 # make pairs and sum shortest distance
 for i in range(len(galaxies)):
     for j in range(i+1,len(galaxies)):
-        total_sum = total_sum + abs(galaxies[i][0]-galaxies[j][0]) + abs(galaxies[i][1]-galaxies[j][1])
+        h_count = 0
+        v_count = 0
+        lower = min(galaxies[i][0], galaxies[j][0])
+        upper = max(galaxies[i][0], galaxies[j][0])
+        for el in vindices:
+            if el in range(lower, upper):
+                h_count = h_count + 1
+        lower = min(galaxies[i][1], galaxies[j][1])
+        upper = max(galaxies[i][1], galaxies[j][1])
+        for el in hindices:
+            if el in range(lower, upper):
+                v_count = v_count + 1
+        distance = abs(galaxies[i][0]-galaxies[j][0]) +h_count*(factor-1) + abs(galaxies[i][1]-galaxies[j][1]) +v_count*(factor-1)
+        total_sum = total_sum + distance
 
 print(total_sum)
