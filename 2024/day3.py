47aa0a6709444c68a6b2bbdff1ae53dcf2b2a13c
stephaniebernhard <fanny.bernhard@gmail.com>
2024-12-03 17:45:15 +0100

    refactor some more

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,23 +1,16 @@
 from pathlib import Path
 import re
 
-INPUT_PATH = Path.cwd() / "inputfiles" / "day3.txt"
-
-def import_input(file_path=INPUT_PATH):
-    with open(file_path, "r") as f:
-        return [line.rstrip('\n') for line in f]
-    
-data = import_input()
+regex_pattern = r"mul\([0-9]{1,3}\,[0-9]{1,3}\)"
+instructions = re.findall(regex_pattern , open(Path.cwd() / "inputfiles/day3.txt").read())
 
 def evaluate(instruction):
     x,y = instruction[4:-1].split(",")
     return int(x) * int(y)
 
-sum = 0 
+sum = 0
 
-for line in data:
-    instructions = re.findall(r"mul\([0-9]{1,3}\,[0-9]{1,3}\)" ,line)
-    for instruction in instructions:
-        sum = sum + evaluate(instruction)
+for instruction in instructions:
+    sum = sum + evaluate(instruction)
 
 print(sum)
--- 2024/day3p2.py
+++ 2024/day3p2.py
@@ -1,16 +1,8 @@
 from pathlib import Path
 import re
 
-INPUT_PATH = Path.cwd() / "inputfiles" / "day3.txt"
-
-def import_input(file_path=INPUT_PATH):
-    with open(file_path, "r") as f:
-        return [line.rstrip('\n') for line in f]
-    
-data = import_input()
-one_string_data = "".join(data)
-
-instructions = re.findall(r"mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don\'t\(\)", one_string_data)
+regex_pattern = r"mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don\'t\(\)"
+instructions = re.findall(regex_pattern , open(Path.cwd() / "inputfiles/day3.txt").read())
 
 def evaluate_mul(instruction):
     x,y = instruction[4:-1].split(",")
